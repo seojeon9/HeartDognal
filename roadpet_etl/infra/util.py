2125ce92bf3d42aef2b75cf24b72fd353503f2ff
seojeon9 <underloose18@gmail.com>
2022-10-19 17:52:14 +0900

    util update

--- roadpet_etl/infra/util.py
+++ roadpet_etl/infra/util.py
@@ -29,18 +29,3 @@ def execute_rest_api(method, url, headers, params):
         raise Exception('응답코드 : ' + str(res.status_code))
 
     return res.text
-
-
-def get_movie_codes():
-    movie_codes = []
-    movie_names = []
-    data = find_data(DataWarehouse, 'MOVIE_DETAIL')
-    # print(type(data))  # pyspark.sql.dataframe.DataFrame
-    data = data.select('MOVIE_CODE', 'MOVIE_NAME')
-    data = data.to_pandas_on_spark()
-    for dt in data.values:
-        print(dt)
-        movie_codes.append(dt[0])
-        movie_names.append(dt[1])
-
-    return movie_codes, movie_names
