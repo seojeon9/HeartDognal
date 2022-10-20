d0a08d907cf3f9fa8ec18a957f2d432b4ec6aeb9
seojeon9 <underloose18@gmail.com>
2022-10-20 17:37:39 +0900

    [시도/시군구/보호소 ETL]

--- roadpet_etl/tests/shelter_test.py
+++ roadpet_etl/tests/shelter_test.py
@@ -1,10 +1,18 @@
 import unittest
+from datajob.etl.extract.shelter_extract import ShelterExtract
 from datajob.etl.extract.sido_extract import SidoExtract
+from datajob.etl.extract.sigungu_extract import SigunguExtract
 
 
 class MTest(unittest.TestCase):
     def test1(self):
         SidoExtract.extract_data()
+
+    def test2(self):
+        SigunguExtract.extract_data()
+
+    def test3(self):
+        ShelterExtract.extract_data()
 
 
 if __name__ == "__main__":
