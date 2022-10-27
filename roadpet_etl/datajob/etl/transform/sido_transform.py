4c99f46afb58fc55b4112e3604f4cb85bb54849d
seojeon9 <underloose18@gmail.com>
2022-10-27 17:33:10 +0900

    [시도/시군구/보호소 ETL] 시도 가공

--- roadpet_etl/datajob/etl/transform/sido_transform.py
+++ roadpet_etl/datajob/etl/transform/sido_transform.py
@@ -7,11 +7,11 @@ class SidoTrasformer:
     @classmethod
     def transform(cls):
         path = '/road_pet/shelter/sido/sido.json'
-        sido_json = get_spark_session().read.option(
-            "multiline", "true").json(path, encoding='UTF-8').first()
+        sido_json = get_spark_session().read.option("multiline", "true").json(path, encoding='UTF-8').first()
 
-        sido_df = get_spark_session().createDataFrame(
-            sido_json['response']['body']['items']['item'])
+        sido_df = get_spark_session().createDataFrame(sido_json['response']['body']['items']['item'])
 
+        sido_df = sido_df.select(sido_df.orgCd.alias('SIDO_CD'), sido_df.orgdownNm.alias('SIDO_NM'))
+        save_data(DataWarehouse, sido_df, 'SIDO')
         # DW테이블에 맞게 가공
         # save_data(DataWarehouse, sido_df, 'ACTORS')
