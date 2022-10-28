98f49e78d67255431afdb7a925de775516045608
seojeon9 <underloose18@gmail.com>
2022-10-28 12:05:56 +0900

    api 품종 extractor

--- roadpet_etl/datajob/etl/extract/kind_api_extract.py
+++ roadpet_etl/datajob/etl/extract/kind_api_extract.py
@@ -5,14 +5,14 @@ from infra.util import execute_rest_api
 class KindExtract:
     URL = 'http://apis.data.go.kr/1543061/abandonmentPublicSrvc/kind'
     SERVICE_KEY = 'NJZf0IxXtTO8vlgpcZ8TbyYzgziNOLkFbn8dWmvTRbx4AYWTjdPnpNd2nbroAcineXLi971rvGbpoy23qSMPmQ=='
-    FILE_DIR = '/road_pet/shelter/sido/'
+    FILE_DIR = '/road_pet/kind/'
 
     @classmethod
     def extract_data(cls):
 
         params = cls.__create_param()
         res = execute_rest_api('get', cls.URL, {}, params)
-        file_name = 'sido.json'
+        file_name = 'kind.json'
         cls.__upload_to_hdfs(file_name, res)
 
     @classmethod
@@ -23,5 +23,6 @@ class KindExtract:
     def __create_param(cls):
         return {
             'serviceKey': cls.SERVICE_KEY,
+            'up_kind_cd': '417000',
             '_type': 'json'
         }
