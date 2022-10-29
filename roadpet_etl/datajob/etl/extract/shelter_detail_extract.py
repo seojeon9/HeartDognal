d8c46416625fb409ffff25c8a002052f164d4cec
seojeon9 <underloose18@gmail.com>
2022-10-29 21:26:16 +0900

    [보호소 ETL] 보호소 상세 정보 추출_1

---
+++ roadpet_etl/datajob/etl/extract/shelter_detail_extract.py
@@ -0,0 +1,90 @@
+import json
+from infra.hdfs_client import get_client
+from infra.jdbc import DataWarehouse, find_data
+from infra.spark_session import get_spark_session
+from infra.util import execute_rest_api
+from infra.logger import get_logger
+
+
+class ShelterDetailExtract:
+    URL = 'http://apis.data.go.kr/1543061/abandonmentPublicSrvc/shelter?'
+    SERVICE_KEY = 'NJZf0IxXtTO8vlgpcZ8TbyYzgziNOLkFbn8dWmvTRbx4AYWTjdPnpNd2nbroAcineXLi971rvGbpoy23qSMPmQ=='
+    FILE_DIR = '/road_pet/shelter/shelter/'
+
+    @classmethod
+    def extract_data(cls):
+        sido_df = find_data(DataWarehouse, 'SIGUNGU')
+        sido_list = sido_df[['SIDO_CD']].collect()
+        sigungu_list = sido_df[['SIGUNGU_CD']].collect()
+
+        for i in range(1):
+            # for i in range(len(sido_list)):
+            sido = str(sido_list[i]['SIDO_CD'])
+            sigungu = str(sigungu_list[i]['SIGUNGU_CD'])
+
+            shelter_df = cls.__generate_df(sido, sigungu)
+            if shelter_df == '':
+                # print(sido)
+                continue
+
+                shelter_df
+
+            shelter_df.show()
+
+        # for i in range(len(sido_list)):
+        #     sido = str(sido_list[i]['SIDO_CD'])
+        #     sigungu = str(sigungu_list[i]['SIGUNGU_CD'])
+        #     # print(sido, sigungu)
+        #     # sido = '6110000'
+        #     # sigungu = '3220000'
+
+        #     try:
+        #         params = cls.__create_param(sido, sigungu)
+        #         res = execute_rest_api('get', cls.URL, {}, params)
+        #         file_name = 'shelter_' + params['upr_cd'] + '_' + params['org_cd'] + '.json'
+        #         cls.__upload_to_hdfs(file_name, res)
+        #     except Exception as e:
+        #         log_dict = cls.__create_log_dict(params)
+        #         cls.__dump_log(log_dict, e)
+        #         raise e
+
+    @classmethod
+    def __upload_to_hdfs(cls, file_name, res):
+        get_client().write(cls.FILE_DIR + file_name, res, encoding='utf-8', overwrite=True)
+
+    @classmethod
+    def __create_param(cls, upr_cd, org_cd):
+        return {
+            'serviceKey': cls.SERVICE_KEY,
+            'upr_cd': upr_cd,
+            'org_cd': org_cd,
+            '_type': 'json',
+        }
+
+    @classmethod
+    def __dump_log(cls, log_dict, e):
+        log_dict['err_msg'] = e.__str__()
+        log_json = json.dumps(log_dict, ensure_ascii=False)
+        get_logger('shelter_extract').error(log_json)
+
+    @classmethod
+    def __create_log_dict(cls, params):
+        log_dict = {
+            'is_success': 'Fail',
+            'type': 'shelter',
+            'params': params
+        }
+
+        return log_dict
+
+    @classmethod
+    def __generate_df(cls, sido, sigungu):
+
+        path = '/road_pet/shelter/shelter/shelter_' + sido + '_' + sigungu + '.json'
+        shelter_json = get_spark_session().read.option("multiline", "true").json(path, encoding='UTF-8').first()
+
+        try:
+            shelter_df = get_spark_session().createDataFrame(shelter_json['response']['body']['items']['item'])
+            return shelter_df
+        except:
+            return ''
