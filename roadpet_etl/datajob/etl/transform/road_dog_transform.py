97b4f46a90cb84793cb0ec26c54bf3beb0be3bfb
Jisu Son <s.zisu0417@gmail.com>
2022-11-09 14:02:00 +0900

    git ignore file fix

--- roadpet_etl/datajob/etl/transform/road_dog_transform.py
+++ roadpet_etl/datajob/etl/transform/road_dog_transform.py
@@ -11,16 +11,17 @@ from infra.util import cal_std_day_yyyymmdd
 class RoadDogTrasformer:
     @classmethod
     def transform(cls):
+        shelter_df = find_data(DataWarehouse, 'SHELTER')
+        shelter_df = shelter_df.dropDuplicates(['CARE_ID'])
+        shelter_df.show()
+        road_dog_pd = pd.DataFrame(columns=['DESERTION_NO','COLOR','SEX','KIND_NM','AGE','WEIGHT','NEUTER_YN','PROCESS_STATE','HAPPEN_DT','HAPPEN_PLACE','SPECIAL_MARK','THUMBNAIL','IMAGE', 'NOTICESDT', 'NOTICEEDT', 'CARE_ID', 'STD_DATE'])
+        road_dog_df = get_spark_session().createDataFrame(road_dog_pd \
+                        , schema = 'DESERTION_NO string, COLOR string, SEX string, KIND_NM string, AGE string, WEIGHT string, NEUTER_YN string, PROCESS_STATE string, HAPPEN_DT string, HAPPEN_PLACE string, SPECIAL_MARK string, THUMBNAIL string, IMAGE string, NOTICESDT string, NOTICEEDT string, CARE_ID string, STD_DATE string')
+        
         for i in range(1, 21):
-            road_dog_pd = pd.DataFrame(columns=['DESERTION_NO','COLOR','SEX','KIND_NM','AGE','WEIGHT' \
-            ,'NEUTER_YN','PROCESS_STATE','HAPPEN_DT','HAPPEN_PLACE','SPECIAL_MARK','PROFILE','ST_CD', 'STD_DATE', 'NOTICE_DT'])
-
-            road_dog_df = get_spark_session().createDataFrame(road_dog_pd \
-                            , schema = 'DESERTION_NO string, COLOR string, SEX string, KIND_NM string, AGE string, WEIGHT string, NEUTER_YN string, PROCESS_STATE string, HAPPEN_DT string, HAPPEN_PLACE string, SPECIAL_MARK string, PROFILE string, ST_CD string, STD_DATE string, NOTICE_DT string')
-
             std_date=str(datetime.now().date()).replace('-','')
-            shelter_df = find_data(DataWarehouse, 'SHELTER')
-            path = '/roadpet/detail/road_dog_' + cal_std_day_yyyymmdd(1) + '.json'
+
+            path = '/roadpet/detail/road_dog_' + cal_std_day_yyyymmdd(i) + '.json'
             road_dog_json = get_spark_session().read.option("multiline","true").option("ignoreLeadingWhiteSpace","true").json(path, encoding='UTF-8').first()
             tmp = get_spark_session().createDataFrame(road_dog_json['response']['body']['items']['item'])
             road_dog_tmp = tmp.withColumn('KIND_NM', split(tmp['kindCd'], ' ', limit=2).getItem(1)) \
@@ -28,7 +29,7 @@ class RoadDogTrasformer:
                                 .withColumn('WEIGHT', split(tmp['weight'], '\\(', limit=2).getItem(0))
             # shelter_df.show()
             # road_dog_tmp.show()
-            road_dog_shelter = road_dog_tmp.join(shelter_df, road_dog_tmp.careTel == shelter_df.CARE_TEL, how="left")
+            road_dog_shelter = road_dog_tmp.join(shelter_df, road_dog_tmp.careTel == shelter_df.CARE_TEL)
             # road_dog_tmp.show()
 
             road_dog_color = road_dog_shelter.withColumn("white", when(col('colorCd').rlike('^.*흰|백|(크림)|(화이트)|(아이보리)|(하양)|(하얀)|(백구)|희|(white).*'),"흰").otherwise(""))\
@@ -36,7 +37,8 @@ class RoadDogTrasformer:
                                 .withColumn("brown", when(col('colorCd').rlike('^.*갈|(브라운)|밤|(초코)|(고동)|(커피)|탄|(쵸코).*'), "갈").otherwise(""))\
                                 .withColumn("grey", when(col('colorCd').rlike('^.*회|(실버)|(그레이).*'), "회").otherwise(""))\
                                 .withColumn("black", when(col('colorCd').rlike('^.*검|흑|(블랙).*'), "검").otherwise(""))\
-                                .withColumn("COLOR", concat('white','gold','brown','grey','black'))\
+                                .withColumn("COLOR_TMP", concat('white','gold','brown','grey','black'))\
+                                .withColumn("COLOR", when(col("COLOR_TMP")=="", "기타").otherwise(col("COLOR_TMP")))\
                                 .withColumn("STD_DATE", lit(std_date))
             
             road_dog = road_dog_color.select(
@@ -45,23 +47,44 @@ class RoadDogTrasformer:
                             ,col('AGE')
                             ,col('WEIGHT')
                             ,col('COLOR')
-                            ,col('ST_CD')
+                            ,col('CARE_ID')
                             ,col('sexCd').alias('SEX')
                             ,col('neuterYn').alias('NEUTER_YN')
                             ,col('processState').alias('PROCESS_STATE')
                             ,col('happenDt').alias('HAPPEN_DT')
                             ,col('happenPlace').alias('HAPPEN_PLACE')
                             ,col('specialMark').alias('SPECIAL_MARK')
-                            ,col('filename').alias('PROFILE')
-                            ,col('STD_DATE')
-                            ,col('noticeSdt').alias('NOTICE_DT'))
+                            ,col('filename').alias('THUMBNAIL')
+                            ,col('popfile').alias('IMAGE')
+                            ,col('noticeSdt').alias('NOTICESDT')
+                            ,col('noticeEdt').alias('NOTICEEDT')
+                            ,col('STD_DATE'))
 
             road_dog_df = road_dog_df.unionByName(road_dog)
         road_dog_df.show()
 
-        road_dog_today = road_dog_df.select("*").where(road_dog_df.NOTICE_DT==std_date)
-        road_dog_today.show()
+        # road_dog_rename = road_dog_df.select(
+        #         col('DESERTION_NO').alias('유기번호')
+        #         ,col('KIND_NM').alias('품종')
+        #         ,col('AGE').alias('나이')
+        #         ,col('WEIGHT').alias('체중')
+        #         ,col('COLOR').alias('색상')
+        #         ,col('SEX').alias('성별')
+        #         ,col('NEUTER_YN').alias('중성화여부')
+        #         ,col('PROCESS_STATE').alias('상태')
+        #         ,col('HAPPEN_DT').alias('접수일')
+        #         ,col('HAPPEN_PLACE').alias('발견장소')
+        #         ,col('SPECIAL_MARK').alias('특징')
+        #         ,col('PROFILE').alias('썸네일')
+        #         ,col('STD_DATE').alias('기준날짜')
+        #     )
+        # road_dog_rename.toPandas().to_csv('./datajob/etl/data/raw_data.csv',encoding = 'utf-8', index = False)
 
-        road_dog_finish = road_dog_df.select("*").where(road_dog_df.PROCESS_STATE.like('종료%'))
-        road_dog_finish.show()
+        save_data(DataWarehouse, road_dog_df, 'ROADDOG_INFO')
+
+        # road_dog_today = road_dog_df.select("*").where(road_dog_df.NOTICE_DT==std_date)
+        # road_dog_today.show()
+
+        # road_dog_finish = road_dog_df.select("*").where(road_dog_df.PROCESS_STATE.like('종료%'))
+        # road_dog_finish.show()
 
