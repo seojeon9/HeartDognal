0fade934177a26a8d39743577853d6fa66fe206d
seojeon9 <underloose18@gmail.com>
2022-11-11 17:05:57 +0900

    [webpage] urls search filter 추가

--- roadpet_webpage/RoadPet/my_road_pet/urls.py
+++ roadpet_webpage/RoadPet/my_road_pet/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('recommend/presurvey/', views.presurvey, name='presurvey'),
     path('recommend/', views.recommend, name='recommend'),
     path('search/', views.search, name='search'),
+    path('search/filter', views.search_filter, name='search_filter'),
     path('detail_info/', views.detail_info, name='detail'),
     path('recommend/survey/submit/', views.survey_submit, name='survey_sub'),
 ]
