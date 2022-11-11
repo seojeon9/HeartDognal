da8df2f84778e13fef8d7548282d0d0c69fa7d2a
seojeon9 <underloose18@gmail.com>
2022-11-11 10:22:15 +0900

    [운영디비 ETL] jdbc operate database 추가

--- roadpet_etl/infra/jdbc.py
+++ roadpet_etl/infra/jdbc.py
@@ -17,12 +17,22 @@ class DataMart(Enum):
     }
 
 
+class OperateDatabase(Enum):
+    URL = 'jdbc:oracle:thin:@roadpet_high?TNS_ADMIN=/home/big/study/db/Wallet_DEMOVIE'
+    PROPS = {
+        'user': 'roadpet_op',
+        'password': '123qwe!@#QWE'
+    }
+
+
 def save_data(config, dataframe, table_name):
-    dataframe.write.jdbc(url=config.URL.value, table=table_name, mode='append', properties=config.PROPS.value)
+    dataframe.write.jdbc(url=config.URL.value, table=table_name,
+                         mode='append', properties=config.PROPS.value)
 
 
 def overwrite_data(config, dataframe, table_name):
-    dataframe.write.jdbc(url=config.URL.value, table=table_name, mode='overwrite', properties=config.PROPS.value)
+    dataframe.write.jdbc(url=config.URL.value, table=table_name,
+                         mode='overwrite', properties=config.PROPS.value)
 
 
 def find_data(config, table_name):
